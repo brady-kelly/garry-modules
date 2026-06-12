f1a1da507419bbb9ea5792560f903e154655f2c5
Brady Kelly <brady_kelly@tutamail.com>
2026-06-12 21:03:39 +0200

    hide notebooks

--- hello_world.py
+++
@@ -1,7 +0,0 @@
-def print_hello_world():
-    print("Hello...")
-    
-def test_print_hello_world(capsys):
-    print_hello_world()
-    captured = capsys.readouterr()
-    assert captured.out == "Hello...\n"
